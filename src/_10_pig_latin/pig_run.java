3e4b3ea429a74aac8154f3dafb9916a8dfae8931
student <student@ip-172-31-52-99.us-west-2.compute.internal>
2021-04-17 11:20:54 -0700

    FWEW

---
+++ src/_10_pig_latin/pig_run.java
@@ -0,0 +1,10 @@
+package _10_pig_latin;
+
+public class pig_run {
+
+public static void main(String[] args) {
+	pig pig = new pig();	
+
+pig.run();
+}
+}
---
+++ src/_11_whack_a_mole/mole.java
@@ -0,0 +1,20 @@
+package _11_whack_a_mole;
+
+import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class mole {
+JFrame frame = new JFrame();
+JPanel panel = new JPanel();
+Random ran = new Random();
+public void run() {
+	frame.add(panel);
+	frame.setVisible(true);
+	frame.pack();
+}
+public void drawButtons() {
+	
+}
+}
